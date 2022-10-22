5548029d3c310e69f4f0013721c2c5af36df2c7d
Willy Oliveira <willy.oliveira@acct.global>
2022-10-22 15:11:08 -0300

    aula 45: Criando minha primeira mutation

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -1,20 +1,34 @@
+// Query resolvers
 const post = async (_, { id }, { dataSources }) => {
   const post = dataSources.postApi.getPost(id);
   return post;
 };
 
 const posts = async (_, { input }, { dataSources }) => {
-  const posts = dataSources.postApi.getPosts(input)
+  const posts = dataSources.postApi.getPosts(input);
   return posts;
 };
 
+// Mutation resolvers
+const createPost = async (_, args, { dataSources }) => {
+    console.log(args);
+    return {
+      id: '601',
+      title: 'Nihil numquam eum iure consequatur.',
+      body: 'Repellendus nam voluptates quis accusantium non similique.',
+      userId: '502',
+      indexRef: 19,
+      createdAt: '2017-04-26T19:39:05.420Z',
+    };
+  };
 
-
+// Field resolver
 const user = async ({ userId }, _, { dataSources }) => {
   return dataSources.userApi.batchLoadById(userId);
 };
 
 export const postResolvers = {
   Query: { post, posts },
+  Mutation: { createPost },
   Post: { user },
 };

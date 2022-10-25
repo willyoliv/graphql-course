eb04294a60b241b7751d09e2b090ce70f5ac068b
Willy Oliveira <willy.oliveira@acct.global>
2022-10-25 20:40:57 -0300

    aula 66: Autorizando atualização apenas para o dono do post (c/ informações sobre cache)

--- src/graphql/post/typedefs.js
+++ src/graphql/post/typedefs.js
@@ -24,12 +24,10 @@ export const postTypeDefs = gql`
   input CreatePostInput {
     title: String!
     body: String!
-    # userId: String!
   }
 
   input UpdatePostInput {
     title: String
     body: String
-    userId: String
   }
 `;

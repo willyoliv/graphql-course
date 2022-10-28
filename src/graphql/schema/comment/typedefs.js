bf8cf9a6d440e47cd933f263857864b54a9c9471
Willy Oliveira <willy.oliveira@acct.global>
2022-10-27 22:05:33 -0300

    aula 86: Typedefs - Definição de tipos dos comentários

---
+++ src/graphql/schema/comment/typedefs.js
@@ -0,0 +1,19 @@
+import { gql } from 'apollo-server';
+
+export const commentTypedefs = gql`
+  extend type Mutation {
+    createComment(data: CreateCommentInput!): Comment!
+  }
+
+  type Comment {
+    id: ID!
+    comment: String!
+    user: User!
+    createdAt: String!
+  }
+
+  input CreateCommentInput {
+    comment: String!
+    postId: String!
+  }
+`;
